ab5a05d5932002e11ce63457757d63735ac0894d
meowrain <meowrain@126.com>
2023-08-17 15:24:07 +0800

    修复projects/item

--- src/app/projects/[item]/page.jsx
+++ src/app/projects/[item]/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./page.module.css";
-import MyButton from "../../../components/btn/button";
 import Image from "next/image";
+import Link from "next/link";
 export default function Category({ params }) {
   return (
     <div className={styles.container}>
@@ -34,7 +34,7 @@ export default function Category({ params }) {
         <div className={styles.content}>
           <h1 className={styles.title}>Test</h1>
           <p className={styles.desc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, veniam! Magni voluptates beatae, provident perspiciatis fuga blanditiis velit vero ipsum aspernatur nam quo illum natus fugit atque dignissimos corporis neque!</p>
-          <MyButton text="See More" url="#"></MyButton>
+          <Link href="#"><button className={styles.btn}></button></Link>
         </div>
         <div className={styles.imgContainer}>
           <Image src="" alt="" fill={true} className={styles.img}></Image>
