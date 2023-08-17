0b2c252b090b86ccead7eb44bf05c51e43c8940c
meowrain <meowrain@126.com>
2023-08-17 15:26:31 +0800

    修复projects/item

--- src/app/projects/[item]/page.jsx
+++ src/app/projects/[item]/page.jsx
@@ -10,7 +10,7 @@ export default function Category({ params }) {
         <div className={styles.content}>
           <h1 className={styles.title}>Test</h1>
           <p className={styles.desc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, veniam! Magni voluptates beatae, provident perspiciatis fuga blanditiis velit vero ipsum aspernatur nam quo illum natus fugit atque dignissimos corporis neque!</p>
-          <MyButton text="See More" url="#"></MyButton>
+          <Link href="#"><button className={styles.btn}>See More</button></Link>
         </div>
         <div className={styles.imgContainer}>
           <Image src="" alt="" fill={true} className={styles.img}></Image>
@@ -22,7 +22,7 @@ export default function Category({ params }) {
         <div className={styles.content}>
           <h1 className={styles.title}>Test</h1>
           <p className={styles.desc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, veniam! Magni voluptates beatae, provident perspiciatis fuga blanditiis velit vero ipsum aspernatur nam quo illum natus fugit atque dignissimos corporis neque!</p>
-          <MyButton text="See More" url="#"></MyButton>
+          <Link href="#"><button className={styles.btn}>See More</button></Link>
         </div>
         <div className={styles.imgContainer}>
           <Image src="" alt="" fill={true} className={styles.img}></Image>
@@ -34,7 +34,7 @@ export default function Category({ params }) {
         <div className={styles.content}>
           <h1 className={styles.title}>Test</h1>
           <p className={styles.desc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, veniam! Magni voluptates beatae, provident perspiciatis fuga blanditiis velit vero ipsum aspernatur nam quo illum natus fugit atque dignissimos corporis neque!</p>
-          <Link href="#"><button className={styles.btn}></button></Link>
+          <Link href="#"><button className={styles.btn}>See More</button></Link>
         </div>
         <div className={styles.imgContainer}>
           <Image src="" alt="" fill={true} className={styles.img}></Image>
